30b25ce0fad461bf18ecb9b7d08c639513e9d4d4
Kebirs <dklecha99@gmail.com>
2021-05-11 01:18:42 +0200

    some text added

--- spoke/about.py
+++ spoke/about.py
@@ -7,6 +7,7 @@ class SecondMenu(Settings, DataWriter):
         super(SecondMenu, self).__init__()
 
     def menu_content_about(self, url):
+        # TODO: Split it ...
         data = {}
         resp = self.get_request(url)
         sec_menu = html.fromstring(resp.text).xpath('//header//text()')
@@ -20,13 +21,51 @@ class SecondMenu(Settings, DataWriter):
 class AboutPage(SecondMenu):
     def __init__(self):
         super(AboutPage, self).__init__()
-        self.scrape_about_content()
+        self.scrape_content()
 
-    def scrape_about_content(self):
+    def scrape_content(self):
         urls = ['https://spoke-london.com/gb/pages/about',
                 'https://spoke-london.com/de/pages/about']
 
         [self.menu_content_about(url) for url in urls]
+        # [self.first_banner(self.get_request(url)) for url in urls]
+        [self.about_page_body(self.get_request(url)) for url in urls]
+
+    def about_page_body(self, url):
+        self.first_banner(url)
+        self.under_first_banner_text(url)
+
+    def first_banner(self, url):
+        data = {}
+        text = html.fromstring(url.text).xpath('//h1[@class="banner__title"]//text()')
+        text = [x.strip() for x in text if x]
+        text = list(filter(None, text))
+        data['First banner'] = text
+
+        self.about_output(data)
+
+    def under_first_banner_text(self, url):
+        data = {}
+
+        title = html.fromstring(url.text).xpath('//h2[@class="blueprint__title"]/text()')
+        title = [x.strip().replace('\n', '').replace('  ', '') for x in title]
+        data['Under first banner blueprint title'] = title
+
+        lines = html.fromstring(url.text).xpath('//p[@class="blueprint__line"]/text()')
+        lines = [x.strip().replace('\n', '').replace('  ', '') for x in lines]
+
+        data['Under first banner blueprint description'] = lines
+
+        self.about_output(data)
+
+    def benefits_banners(self, url):
+        pass
+
+    def support_divs(self, url):
+        pass
+
+    def social_media_title(self, url):
+        pass
 
 
 if __name__ == '__main__':
