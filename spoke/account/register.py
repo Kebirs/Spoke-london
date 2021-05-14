ae7a671ce8d7cfe8f511cabf3224fbc742040cc0
Kebirs <dklecha99@gmail.com>
2021-05-14 15:33:22 +0200

    Register page done

--- spoke/account/register.py
+++ spoke/account/register.py
@@ -16,5 +16,41 @@ class RegisterPage(Settings, DataWriter):
     def register_page_body(self, url):
         data = {}
 
+        root = "//form[@id='create_customer']"
 
-        self.newsletter_output(data)
+        title = f"{root}//h2[@class='form__title']//text()"
+        byline = f"{root}//p[@class='form__byline']//text()"
+
+        first_name_hint = f"{root}//input[@name='customer[first_name]']/@placeholder"
+        last_name_hint = f"{root}//input[@name='customer[last_name]']/@placeholder"
+        email_hint = f"{root}//input[@name='customer[email]']/@placeholder"
+        password_hint = f"{root}//input[@name='customer[password]']/@placeholder"
+
+        required_fields = f"{root}//div[@class='grid'][1]//text()"
+        over_button_notes = f"{root}//div[contains(@class, 'gdpr-fields')]//text()"
+        button = f"{root}//span[@class='button__content']//text()"
+
+        title = html.fromstring(url.text).xpath(title)
+        byline = html.fromstring(url.text).xpath(byline)
+        first_name_hint = html.fromstring(url.text).xpath(first_name_hint)
+        last_name_hint = html.fromstring(url.text).xpath(last_name_hint)
+        email_hint = html.fromstring(url.text).xpath(email_hint)
+        password_hint = html.fromstring(url.text).xpath(password_hint)
+        required_fields = html.fromstring(url.text).xpath(required_fields)
+        over_button_notes = html.fromstring(url.text).xpath(over_button_notes)
+        button = html.fromstring(url.text).xpath(button)
+
+        properties = {'Title': title,
+                      'Byline': byline,
+                      'First name hint': first_name_hint,
+                      'Last name hint': last_name_hint,
+                      'Email hint': email_hint,
+                      'Password hint': password_hint,
+                      'Required fields': required_fields,
+                      'Over button notes': over_button_notes,
+                      'Button Text': button}
+
+        for k, v in properties.items():
+            data[k] = self.clean_data(v)
+
+        self.register_output(data)
