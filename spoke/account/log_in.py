466c124319824ba9f030afca2c5907e5e4091b52
Kebirs <dklecha99@gmail.com>
2021-05-14 15:06:14 +0200

    Log in Page done

--- spoke/account/log_in.py
+++ spoke/account/log_in.py
@@ -16,5 +16,36 @@ class LogInPage(Settings, DataWriter):
     def log_in_page_body(self, url):
         data = {}
 
+        root = "//form[@id='customer_login']"
 
-        self.newsletter_output(data)
+        title = f"{root}//h2[@class='form__title']//text()"
+        byline = f"{root}//p[@class='form__byline']//text()"
+        email_hint = f"{root}//input[@name='customer[email]']/@placeholder"
+        password_hint = f"{root}//input[@name='customer[password]']/@placeholder"
+        email_error = f"{root}//p[contains(@class, 'form__error')]//text()"
+        button = f"{root}//span[@class='button__content']//text()"
+        forgotten_pass = f"{root}//p[@class='form__link']//text()"
+        register = f"{root}//p[contains(@class, 'form__link--separator')]//text()"
+
+        title = html.fromstring(url.text).xpath(title)
+        byline = html.fromstring(url.text).xpath(byline)
+        email_hint = html.fromstring(url.text).xpath(email_hint)
+        password_hint = html.fromstring(url.text).xpath(password_hint)
+        email_error = html.fromstring(url.text).xpath(email_error)
+        button = html.fromstring(url.text).xpath(button)
+        forgotten_pass = html.fromstring(url.text).xpath(forgotten_pass)
+        register = html.fromstring(url.text).xpath(register)
+
+        properties = {'Title': title,
+                      'Byline': byline,
+                      'Email hint': email_hint,
+                      'Password hint': password_hint,
+                      'Email Error Message': email_error,
+                      'Button Text': button,
+                      'Forgotten Password Text': forgotten_pass,
+                      'Register Text': register}
+
+        for k, v in properties.items():
+            data[k] = self.clean_data(v)
+
+        self.log_in_output(data)
